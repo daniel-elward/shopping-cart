d7f02839081c5a9e5584f7bbacc7556e0c18eaba
daniel-elward <daniel.elward@hotmail.com>
2026-04-15 16:53:24 -0300

    added: delete item from cart

--- src/components/pages/cartPage/CartCard.jsx
+++ src/components/pages/cartPage/CartCard.jsx
@@ -1,10 +1,21 @@
 import style from "./CartCard.module.css";
+import { useOutletContext } from "react-router";
 
 export default function CartCard({ product }) {
-  console.log(product);
+  const [cart, setCart] = useOutletContext();
+
+  const handleDelete = (idToDelete) => {
+    setCart((prevItems) => prevItems.filter((item) => item.id !== idToDelete));
+  };
   return (
     <div className={style.productCard}>
       <img src={product.iconLink} alt="" />
+      <button
+        onClick={() => {
+          handleDelete(product.id);
+        }}>
+        delete
+      </button>
       <p>Product: {product.name}</p>
       <p>Price: &#8381; {product.basePrice}</p>
       <p>Quantity: {product.quantity}</p>
